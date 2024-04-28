b4eac17ea86439abf5d4a16c8956d6d9fcddae05
Pratik-Kumar2402 <pratik.kumar2402@gmail.com>
2024-04-28 12:03:30 +0530

    added navbar, contact, about

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,15 +1,18 @@
 from django import forms
 from .models import Image
 
+
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = '__all__'
-        labels = {'photo':''}
-        
+        fields = "__all__"
+        labels = {"photo": ""}
+
+
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=30)
     password = forms.CharField(widget=forms.PasswordInput)
+
 
 class RegisterForm(forms.Form):
     username = forms.CharField(max_length=30)
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('get-session/', views.get_session, name='get_session'),
-    path('login/',views.login, name='login'),
-    path('register/',views.register, name='register'),
+    path("", views.home, name="home"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
 ]
